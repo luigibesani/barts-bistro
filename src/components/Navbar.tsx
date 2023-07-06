1998cb6107414c45686d8f53feb3a63e40a3fdf1
luigibesani <luigi.besani@gmail.com>
2023-07-06 13:16:47 -0400

    Bring menu ahead on z-axis

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,45 @@ export const Navbar = () => {
   return (
     <div className="navbar bg-base-100 ">
       <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden" >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="dropdown z-30">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
-            <li><a>Home</a></li>
-            <li><a>Menu</a></li>
-            <li><a>Catering</a></li>
-            <li><a>Gallery</a></li>
-            <li><a>About Us</a></li>
-            <li><a>Contact</a></li>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Menu</a>
+            </li>
+            <li>
+              <a>Catering</a>
+            </li>
+            <li>
+              <a>Gallery</a>
+            </li>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
           </ul>
         </div>
         <div>
@@ -38,16 +66,27 @@ export const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 text-3xl">
-          <li><a>Home</a></li>
-          <li><a>Menu</a></li>
-          <li><a>Catering</a></li>
-          <li><a>Gallery</a></li>
-          <li><a>About Us</a></li>
-          <li><a>Contact</a></li>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Menu</a>
+          </li>
+          <li>
+            <a>Catering</a>
+          </li>
+          <li>
+            <a>Gallerry</a>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
         </ul>
       </div>
-      <div className="navbar-end">
-      </div>
+      <div className="navbar-end"></div>
     </div>
     // <div className="py-10">
     //   <div className="grid justify-items-center">
